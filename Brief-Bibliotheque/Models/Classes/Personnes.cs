1c9548e018e431bb563e3a98701a3889257b6612
SymBiios <julien.sarkarin@gmail.com>
2025-07-23 15:11:02 +0200

    refacto: changement des classes pour les lier avec framework core

---
+++ Brief-Bibliotheque/Models/Classes/Personnes.cs
@@ -0,0 +1,14 @@
+﻿using Brief_Bibliotheque.Models.Classes.Enums;
+using System.Text.Json.Serialization;
+
+namespace Brief_Bibliotheque.Models.Classes
+{
+    public abstract class Personnes
+    {
+        public int IdPersonnes { get; set; }
+        public required string Nom { get; set; }
+        public required string Prenom { get; set; }
+        public required DateTime DateDeNaissance { get; set; }
+
+    }
+}
--- Brief-Bibliotheque/Models/Classes/Utilisateurs.cs
+++ Brief-Bibliotheque/Models/Classes/Utilisateurs.cs
@@ -3,19 +3,20 @@ using System.Text.Json.Serialization;
 
 namespace Brief_Bibliotheque.Models.Classes
 {
-    public class Utilisateurs : Personne
+    public class Utilisateurs : Personnes
     {
-        public int IdUtilisateur { get; set; }
-        public string Tel { get; set; }
-        public string NumeroDeRue { get; set; }
-        public string NomDeRue { get; set; }
+        public int IdUtilisateurs { get; set; }
+        public required string Tel { get; set; }
+        public required string NumeroDeRue { get; set; }
+        public required string NomDeRue { get; set; }
 
         [JsonConverter(typeof(JsonStringEnumConverter))]
-        public Role Role { get; set; }
-        public string MotDePasse { get; set; }
-        public string Mail { get; set; }
-        public string Ville { get; set; }
-        public string CodePostal { get; set; }
+        public required Role Role { get; set; }
+        public required string MotDePasse { get; set; }
+        public required string Mail { get; set; }
+        public required string Ville { get; set; }
+        public required string CodePostal { get; set; }
 
+        public List<Reservations> Reservations { get; set; } = new List<Reservations>();
     }
 }
