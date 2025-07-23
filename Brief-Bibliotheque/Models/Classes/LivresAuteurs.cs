2d8552d64cd4157f89ebbc77a1d983089602ee06
Loicwr <nephtyse19@hotmail.fr>
2025-07-23 12:12:35 +0200

    feat (classes) : ajout de la classe LivreAuteurs

---
+++ Brief-Bibliotheque/Models/Classes/LivresAuteurs.cs
@@ -0,0 +1,10 @@
+﻿namespace Brief_Bibliotheque.Models.Classes
+{
+    public class LivresAuteurs
+    {
+        public int LivreId { get; set; }
+        public int AuteurId { get; set; }
+        public Livre Livre { get; set; }
+        public Auteur Auteur { get; set; }
+    }
+}
