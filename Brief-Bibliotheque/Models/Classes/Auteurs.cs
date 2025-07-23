ef2ed4a82f98786f806def8be54b65f169f23391
Loicwr <nephtyse19@hotmail.fr>
2025-07-23 13:34:12 +0200

    feat (classes) : ajout classe Auteurs

---
+++ Brief-Bibliotheque/Models/Classes/Auteurs.cs
@@ -0,0 +1,7 @@
+﻿namespace Brief_Bibliotheque.Models.Classes
+{
+    public class Auteurs
+    {
+        public required List<LivresAuteurs> LivresAuteurs { get; set; } = new List<LivresAuteurs>();
+    }
+}
