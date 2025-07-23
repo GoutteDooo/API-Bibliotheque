fd47d9c06cef6360411f0314a476895d3b07f999
GoutteDooo <goudal.Donovan@live.fr>
2025-07-23 09:49:37 +0200

    Ajoutez des fichiers projet.

---
+++ Brief-Bibliotheque/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Brief_Bibliotheque.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
