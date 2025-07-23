a9770667adc934a930c1c8dbed1888373ec4a189
Loicwr <nephtyse19@hotmail.fr>
2025-07-23 12:14:47 +0200

    feat (classes) : ajout de la classe LivreGenres

---
+++ Brief-Bibliotheque/Models/Classes/LivresGenres.cs
@@ -0,0 +1,10 @@
+﻿namespace Brief_Bibliotheque.Models.Classes
+{
+    public class LivresGenres
+    {
+        public int LivreId { get; set; }
+        public int GenreId { get; set; }
+        public Livre Livre { get; set; }
+        public Genre Genre{ get; set; }
+    }
+}
