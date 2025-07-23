81bdb7f7476806bd103f5afec6a24d7a989aa70b
SymBiios <julien.sarkarin@gmail.com>
2025-07-23 13:47:59 +0200

    feat: Création abstract class personne

---
+++ Brief-Bibliotheque/Models/Classes/Personne.cs
@@ -0,0 +1,9 @@
+﻿namespace Brief_Bibliotheque.Models.Classes
+{
+    public abstract class Personne
+    {
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public DateTime DateDeNaissance { get; set; }
+    }
+}
