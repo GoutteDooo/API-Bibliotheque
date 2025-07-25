ef622e2034e80b8f45905d7e700fb90b0addb8eb
GoutteDooo <goudal.donovan@live.fr>
2025-07-25 14:40:39 +0200

    chore(layout): ajout des liens vers les tables dans les vues

--- Brief-Bibliotheque/Models/Classes/LivresAuteurs.cs
+++ Brief-Bibliotheque/Models/Classes/LivresAuteurs.cs
@@ -1,12 +1,11 @@
-﻿/*namespace Brief_Bibliotheque.Models.Classes
+﻿namespace Brief_Bibliotheque.Models.Classes
 {
     public class LivresAuteurs
     {
+        int Id {  get; set; }
         public int IdLivres { get; set; }
         public int IdAuteur { get; set; }
         public required Livres Livre { get; set; }
         public required Auteurs Auteur { get; set; }
-
     }
 }
-*/
--- Brief-Bibliotheque/Models/Classes/LivresGenres.cs
+++ Brief-Bibliotheque/Models/Classes/LivresGenres.cs
@@ -1,12 +1,11 @@
-﻿/*namespace Brief_Bibliotheque.Models.Classes
+﻿namespace Brief_Bibliotheque.Models.Classes
 {
     public class LivresGenres
     {
+        int Id { get; set; }
         public int IdLivres { get; set; }
         public int IdGenres { get; set; }
         public required Livres Livre { get; set; }
-        public required Genre Genre{ get; set; }
+        public required Genres Genre{ get; set; }
     }
-
 }
-*/
