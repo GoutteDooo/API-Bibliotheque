ef8282f61800761ad9e5f16f232c7939dcefed2d
GoutteDooo <goudal.Donovan@live.fr>
2025-07-27 10:52:18 +0200

    refactor(Model/Personne): Nom set en Uppercase directement lors de l'instanciation

--- Brief-Bibliotheque/Models/Classes/Personne.cs
+++ Brief-Bibliotheque/Models/Classes/Personne.cs
@@ -4,8 +4,9 @@ namespace Brief_Bibliotheque.Models.Classes
 {
     public abstract class Personne
     {
+        private string _nom = string.Empty;
         public int Id { get; set; }
-        public required string Nom { get; set; }
+        public required string Nom { get => _nom; set => _nom = value.ToUpperInvariant(); } // Invariant est plus sûr (voir doc pour détails)
         public required string Prenom { get; set; }
         [Display(Name = "Date de naissance")]
         [DataType(DataType.Date)]
