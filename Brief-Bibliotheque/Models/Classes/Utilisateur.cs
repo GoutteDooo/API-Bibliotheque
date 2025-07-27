b063f5f9f3fd744200151603472861670d66ba16
GoutteDooo <goudal.Donovan@live.fr>
2025-07-27 09:13:00 +0200

    chore(Model/Utilisateur): Ajout de validations de données côté serveur

--- Brief-Bibliotheque/Models/Classes/Utilisateur.cs
+++ Brief-Bibliotheque/Models/Classes/Utilisateur.cs
@@ -8,19 +8,26 @@ namespace Brief_Bibliotheque.Models.Classes
     {
         public int UtilisateurId { get; set; }
         [Display(Name = "Téléphone")]
-        public required string Tel { get; set; }
+        [StringLength(14, MinimumLength = 8)]
+        public string? Tel { get; set; }
         [Display(Name = "Numéro de Rue")]
-        public required string NumeroDeRue { get; set; }
+        public string? NumeroDeRue { get; set; }
         [Display(Name = "Nom de Rue")]
-        public required string NomDeRue { get; set; }
+        [StringLength(100, MinimumLength = 6)]
+        public string? NomDeRue { get; set; }
 
         [JsonConverter(typeof(JsonStringEnumConverter))]
         public required Role Role { get; set; }
+        [Required]
+        [StringLength(50)]
         public required string MotDePasse { get; set; }
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$")]
+        [StringLength(70, MinimumLength = 9)]
         public required string Mail { get; set; }
-        public required string Ville { get; set; }
+        public string? Ville { get; set; }
         [Display(Name = "Code Postal")]
-        public required string CodePostal { get; set; }
+        [StringLength(5)]
+        public string? CodePostal { get; set; }
 
         public List<Reservation> Reservations { get; set; } = new List<Reservation>();
     }
