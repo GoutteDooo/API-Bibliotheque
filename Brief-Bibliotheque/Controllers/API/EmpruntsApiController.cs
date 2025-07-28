29a0c845351df4b73ae1330fff95da10aeb914f4
Loicwr <Nephtyse19@hotmail.fr>
2025-07-28 12:44:30 +0200

    feat (API) : ajout de EmpruntApiController

---
+++ Brief-Bibliotheque/Controllers/API/EmpruntsApiController.cs
@@ -0,0 +1,78 @@
+﻿using Brief_Bibliotheque.Models.Classes;
+using Brief_Bibliotheque.Models.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.EntityFrameworkCore;
+
+namespace Brief_Bibliotheque.Controllers.API
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class EmpruntsApiController : ControllerBase
+    {
+        private readonly BiblioDB _context;
+
+        public EmpruntsApiController(BiblioDB context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Emprunt>>> GetEmprunts()
+        {
+            return await _context.Emprunts.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Emprunt>> GetEmprunt(int id)
+        {
+            var emprunt = await _context.Emprunts.FindAsync(id);
+
+            if (emprunt == null)
+            {
+                return NotFound();
+            }
+
+            return emprunt;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Emprunt>> PostEmprunt(Emprunt emprunt)
+        {
+            _context.Emprunts.Add(emprunt);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetEmprunt), new { id = emprunt.Id }, emprunt);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Emprunt>> PutEmprunt(int id, Emprunt emprunt)
+        {
+            if (id != emprunt.Id)
+                return BadRequest();
+            _context.Entry(emprunt).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Emprunts.Any(e => e.Id == id))
+                    return NotFound();
+                throw;
+            }
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Emprunt>> DeleteEmprunt(int id)
+        {
+            var emprunt = await _context.Emprunts.FindAsync(id);
+            if (emprunt == null)
+                return NotFound();
+            _context.Emprunts.Remove(emprunt);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
