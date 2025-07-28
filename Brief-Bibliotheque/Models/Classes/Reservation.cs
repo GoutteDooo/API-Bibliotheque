aebf6ed229abc21e1472692e606f5bb7ffd6249b
GoutteDooo <goudal.donovan@live.fr>
2025-07-28 14:13:11 +0200

    refactor(Reservation): remise en forme des bons nommages d'affichages

--- Brief-Bibliotheque/Models/Classes/Reservation.cs
+++ Brief-Bibliotheque/Models/Classes/Reservation.cs
@@ -9,12 +9,15 @@ namespace Brief_Bibliotheque.Models.Classes
         [Display(Name = "Date de la réservation")]
         [DataType(DataType.Date)]
         public required DateTime DateReservation { get; set; }
+        [Display(Name = "Date de fin de réservation")]
+        [DataType(DataType.Date)]
+        public DateTime DateFinReservation { get; set; }
         [Display(Name = "Terminé")]
         public bool EstTermine { get; set; } = false;
-        public int IdUtilisateur { get; set; }
-        [Display(Name = "ID du Livre")]
-        public Utilisateur? Utilisateur { get; set; }
         [Display(Name = "ID du Membre")]
+        public int IdUtilisateur { get; set; }
+        public Utilisateur? Utilisateur { get; set; }
+        [Display(Name = "ID du Livre")]
         public int IdLivre { get; set; }
         [Display(Name = "Membre")]
         public Livre? Livre { get; set; }
