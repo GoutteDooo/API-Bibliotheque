a42d026883ddb2e891f23fac14f8df4716064206
GoutteDooo <goudal.donovan@live.fr>
2025-07-28 15:01:53 +0200

    feat(view/Employe): Ajout d'une vue Employé

--- Brief-Bibliotheque/Controllers/HomeController.cs
+++ Brief-Bibliotheque/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace Brief_Bibliotheque.Controllers
             return View();
         }
 
+        public IActionResult Employe(int id)
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
