e8afc81a146d844fe92bc107c7089246f87d64b1
Loicwr <Nephtyse19@hotmail.fr>
2025-07-28 12:07:07 +0200

    feat (API) : ajout de LivreApiController

--- Brief-Bibliotheque/Controllers/API/LivresApiController.cs
+++ Brief-Bibliotheque/Controllers/API/LivresApiController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Brief_Bibliotheque.Models.Classes;
+using Brief_Bibliotheque.Models.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Brief_Bibliotheque.Controllers.API
 {
@@ -6,6 +9,64 @@ namespace Brief_Bibliotheque.Controllers.API
     [ApiController]
     public class LivresApiController : ControllerBase
     {
+        private readonly BiblioDB _context;
 
+        public LivresApiController(BiblioDB context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Livre>>> GetLivres()
+        {
+            return await _context.Livres.ToListAsync();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Livre>> GetLivres(int id)
+        {
+            var livre = await _context.Livres.FindAsync(id);
+
+            if (livre == null)
+            {
+                return NotFound();
+            }
+
+            return livre;
+        }
+        [HttpPost]
+        public async Task<ActionResult<Livre>> PostAuteur(Livre livre)
+        {
+            _context.Livres.Add(livre);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetLivres), new { id = livre.Id }, livre);
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutLivre(int id, Livre livre)
+        {
+            if (id != livre.Id)
+                return BadRequest();
+            _context.Entry(livre).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Livres.Any(e => e.Id == id))
+                    return NotFound();
+                throw;
+            }
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteLivre(int id)
+        {
+            var livre = await _context.Livres.FindAsync(id);
+            if (livre == null)
+                return NotFound();
+            _context.Livres.Remove(livre);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
     }
 }
