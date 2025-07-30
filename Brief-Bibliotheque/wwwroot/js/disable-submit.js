834bdc4d3ea700801c7617cffee18abcd2607ca9
GoutteDooo <goudal.donovan@live.fr>
2025-07-30 10:58:35 +0200

    feat(front/script): Ajout d'un script côté front pour éviter que l'user clique plusieur fois sur le bouton "create" et n'envoie plusieurs requêtes post d'un coup

---
+++ Brief-Bibliotheque/wwwroot/js/disable-submit.js
@@ -0,0 +1,14 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const forms = document.querySelectorAll("form");
+
+    forms.forEach(form => {
+        const submitButton = form.querySelector("button[type='submit']");
+        if (!submitButton) return;
+
+        form.addEventListener("submit", function () {
+            // Empêche le double envoi
+            submitButton.disabled = true;
+            submitButton.innerText = "En cours...";
+        });
+    });
+});
