bfa8b3e3ce25762f83fc98a32c70ec4b51f4309e
GoutteDooo <goudal.donovan@live.fr>
2025-08-01 12:26:59 +0200

    feat(animation): bienvenue

--- Brief-Bibliotheque/wwwroot/js/site.js
+++ Brief-Bibliotheque/wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+setTimeout(() => {
+    document.querySelector("#welcome-filter").remove();
+}, 5000)
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    const hasPlayed = localStorage.getItem('welcomePlayed');
+    const welcome = document.getElementById('welcome-filter');
+
+    if (!hasPlayed && welcome) {
+        welcome.classList.add('animate');
+        localStorage.setItem('welcomePlayed', 'true');
+    } else {
+        // L’animation ne se joue pas, on cache le filtre si besoin
+        welcome.style.display = 'none';
+    }
+});
